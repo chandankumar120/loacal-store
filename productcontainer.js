7632cffbb671d146c2239e4459a7c572d0106430
chandan kumar <chandansharma642@gmail.com>
2022-12-15 20:07:21 +0530

    modified javascript page

--- productcontainer.js
+++ productcontainer.js
@@ -1,4 +1,3 @@
-// let parent = document.querySelector(".container");
 const button = document.getElementById("btn");
 
 function createBlock(classname, cotent) {
@@ -7,13 +6,31 @@ function createBlock(classname, cotent) {
   div.innerHTML = cotent;
   return div;
 }
-function addOuter() {
-  let outerelement = document.createElement("div");
-  outerelement.setAttribute("class", "items");
-  document.querySelector(".container").appendChild(outerelement);
 
-  outerelement.appendChild(createBlock("img", "image"));
-  outerelement.appendChild(createBlock("name", "aalu"));
-  outerelement.appendChild(createBlock("price", "5"));
-  outerelement.appendChild(createBlock("info", "this is new"));
-}
+var ajax = new XMLHttpRequest();
+ajax.open("GET", "Database.php", true);
+ajax.send();
+
+ajax.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(this.responseText);
+    console.log(data);
+
+    for (var a = 0; a < data.length; a++) {
+      var firstName = data[a].name;
+      var lastName = data[a].price;
+      var jobTitle = data[a].info;
+
+      // function addOuter() {
+      let outerelement = document.createElement("div");
+      outerelement.setAttribute("class", "items");
+      document.querySelector(".container").appendChild(outerelement);
+
+      outerelement.appendChild(createBlock("img", "image"));
+      outerelement.appendChild(createBlock("name", firstName));
+      outerelement.appendChild(createBlock("price", lastName));
+      outerelement.appendChild(createBlock("info", jobTitle));
+      // }
+    }
+  }
+};
